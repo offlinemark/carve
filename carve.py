0cc88989c8d20ca486f1a36c3c402bf9cdeced90
Mark Mossberg <mark.mossberg@gmail.com>
2013-10-26 12:50:32 -0400

    fix bugs, implement plist scrape
    - Now it works more fully, and scrapes plist files in addition to sqlite
    database.

--- carve.py
+++ carve.py
@@ -10,10 +10,10 @@ root_dir = '/Users/mark/code/sec/forensics/ciphertech/iOS4_logical_acquisition/'
 root_output_dir = '/Users/mark/code/sec/forensics/ciphertech/carvings/'
 # targets = ['Library', 'Address Book', 'Calendar', 'Cookies', 'Mail', 'Maps', 'Safari', 'SMS', 'Voicemail']
 # targets = ['./mobile/Library/AddressBook', './mobile/Library/Calendar']
-targets = ['AddressBook', 'Calendar']
+targets = ['AddressBook', 'Calendar', 'Notes', 'Safari', 'SMS', 'Cookies']
 
-def die():
-    sys.exit()
+def die(msg=''):
+    sys.exit(msg)
 
 ### main ##########
 
@@ -21,11 +21,9 @@ def main():
     os.chdir(root_dir);
     os.mkdir(root_output_dir)
 
-    print os.getcwd()
     for root, dirs, files in os.walk('.'):
         # print dirs
         # if (root in targets) or (root in dirs)
-        # print dirs
         for d in dirs:
             if d in targets:
                 # print d
@@ -33,19 +31,25 @@ def main():
                 # print root
                 # print files
                 output_dir = root_output_dir + d
-                os.mkdir(output_dir)
+                try:
+                    os.mkdir(output_dir)
+                except OSError:
+                    continue
                 # os.chdir(root)
                 # print os.listdir(root + "/" + d)
-                print os.getcwd()
-                print root
+                print os.listdir(root + "/" + d)
                 for f in os.listdir(root + "/" + d):
                     cmd = "file %s" % (root + "/" + d + "/" + f)
                     print cmd
                     cmd_obj = subprocess.Popen(cmd, stdout=subprocess.PIPE, shell=True)
                     cmd_output = cmd_obj.communicate()[0]
                     print cmd_output
-                    if 'SQLite' in cmd_output:
+                    if ('SQLite' in cmd_output) or ('Apple binary' in cmd_output) or ('XML' in cmd_output):
+                        print cmd_output
                         shutil.copy(root + "/" + d + "/" + f, output_dir) 
+                        print root + "/" + d + "/" + f + "   =>  " + output_dir
+                        print
+                        print
 
 
 if __name__ == '__main__':
